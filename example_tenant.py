a0adcdd3b231bbb31fce7feb5ccf4e7b26d8771e
Vishal <31079436+vpralhad@users.noreply.github.com>
2020-02-24 13:58:44 -0700

    Add files via upload

---
+++ example_tenant.py
@@ -0,0 +1,121 @@
+# import libraries
+from credentials import *
+from acitoolkit.acitoolkit import *
+import requests
+import ciscosparkapi
+
+# create session with apic
+session = Session(URL, LOGIN, PASSWORD)
+session.login()
+
+#create tenant and vrf
+tenant_name = "INITIALS_Example_Tenant"
+tenant = Tenant(tenant_name)
+vrf = Context("Example_VRF", tenant)
+
+# create bridge domain with vrf relationship
+bridge_domain = BridgeDomain("Example_BD", tenant)
+bridge_domain.add_context(vrf)
+
+# create public subnet and assign gateway
+subnet = Subnet("Example_Subnet", bridge_domain)
+subnet.set_scope("public")
+subnet.set_addr("10.10.10.1/24")
+
+# create http filter and filter entry
+filter_http = Filter("http", tenant)
+filter_entry_tcp80 = FilterEntry("tcp-80", filter_http, etherT="ip", prot="tcp", dFromPort="http", dToPort="http")
+
+# create sql filter and filter entry
+filter_sql = Filter("sql", tenant)
+filter_entry_tcp1433 = FilterEntry("tcp-1433", filter_sql, etherT="ip", prot="tcp", dFromPort="1433", dToPort="1433")
+
+# create web contract and associate to http filter
+contract_web = Contract("web", tenant)
+contract_subject_http = ContractSubject("http", contract_web)
+contract_subject_http.add_filter(filter_http)
+
+# create database contract and associate to sql filter
+contract_database = Contract("database", tenant)
+contract_subject_sql = ContractSubject("sql", contract_database)
+contract_subject_sql.add_filter(filter_sql)
+
+# create application profile
+app_profile = AppProfile("Example_App", tenant)
+
+# create web epg and associate bridge domain and contracts
+epg_web = EPG("Web", app_profile)
+epg_web.add_bd(bridge_domain)
+epg_web.provide(contract_web)
+epg_web.consume(contract_database)
+
+# create db epg and associate bridge domain and contract
+epg_database = EPG("Database", app_profile)
+epg_database.add_bd(bridge_domain)
+epg_database.provide(contract_database)
+
+# collect list of tenants
+tenant_list = Tenant.get(session)
+
+# print list of tenants
+tenant_list
+for tn in tenant_list:
+    print(tn.name)
+
+# print url and configuration data
+print("\n{}\n\n{}".format(tenant.get_url(), tenant.get_json()))
+
+# neatly print configuration data
+import json
+print(json.dumps(tenant.get_json(), sort_keys=True, indent=2, separators=(',',':')))
+
+# push configuration to apic
+resp = session.push_to_apic(tenant.get_url(), data=tenant.get_json())
+
+# test configuration request
+if resp.ok:
+     print("\n{}: {}\n\n{} is ready for use".format(resp.status_code, resp.reason, tenant.name))
+else:
+     print("\n{}: {}\n\n{} was not created!\n\n Error: {}".format(resp.status_code, resp.reason, subnet.name, resp.content))
+
+
+
+token = "MjYxZThiZWEtMGI5Ny00MGYyLTkzMWYtMDgwMTlkYzQwM2M3MWJmMjUzNDAtMjRi_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
+room_id = "Y2lzY29zcGFyazovL3VzL1JPT00vYTVjMGM1MjAtNzk4Ny0xMWU4LTgwMzctN2I3NDhmZmFiYTZm"
+
+url = "https://api.ciscospark.com/v1/memberships"
+
+payload = "{\r\n  \"roomId\" : \"room_id\",\r\n  \"personEmail\": \"vishalpatil86@rediffmail.com\",\r\n  \"isModerator\": \"false\"\r\n}"
+headers = {
+  'Authorization': 'Bearer TOKEN',
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data = payload)
+print(response.text.encode('utf8'))
+
+def webex_message(item):
+    spark = ciscosparkapi.CiscoSparkAPI(access_token=token)
+    message = spark.messages.create(roomId=room_id, text="{0:30}".format(item.name))
+
+# re-check tenant list
+new_tenant_list = Tenant.get(session)
+print("Below is the list of Tenants -\n")
+for tn in new_tenant_list:
+    print(tn.name)
+    webex_message(tn)
+#check app list in new tenant
+app_list = AppProfile.get(session, tenant)
+print("Below is the Appl List - \n")
+for app in app_list:
+    print(app.name)
+    webex_message(app)
+    print("\n")
+# check epg list in new app
+epg_list = EPG.get(session, app_profile, tenant)
+print("Below is the List of EPGs Created - \n")
+for epg in epg_list:
+    print(epg.name)
+    webex_message(epg)
+# exit
+exit()
